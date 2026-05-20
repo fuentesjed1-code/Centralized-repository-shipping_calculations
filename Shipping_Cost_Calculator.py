4bac07d460e8c1794f474948d14070de3ba13441
Jed <fuentesjed1@gmail.com>
2026-05-20 16:48:08 -0400

    Revert "added another update line to Shipping_Cost_Calculator.py"
    This reverts commit 4061c2f3ec0eaf7b2528682fc60ce255aab51161.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} CAD")
 
-# Here is a new update by fuentesjed1
-# Here is another update by fuentesjed1
+# Here is a new update by <fuentesjed1>
